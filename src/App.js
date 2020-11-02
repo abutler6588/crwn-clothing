6143fda63e9aab2c4c07ecc8bcad1228cc0d8da4
abutler6588 <andrebutler@Andres-Air.attlocal.net>
2020-11-01 18:11:53 -0600

    added Redux, and implemented userReducer and userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -8,19 +8,14 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -28,15 +23,14 @@ class App extends React.Component {
         //if the snapshot has changed
         //set the state
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+        setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
+            });
           });
-      });
     }
-      this.setState({ currentUser: userAuth });
+
+      setCurrentUser(userAuth);
     });
   }
 
@@ -47,7 +41,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-      <Header currentUser={this.state.currentUser} />
+      <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -58,4 +52,12 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  null, 
+  mapDispatchToProps
+  )
+  (App);
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,21 @@
+const INITIAL_STATE = {
+    currentUser: null
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    //depending on what the action is
+    switch (action.type) {
+        //check if case if equal to set current user
+        case 'SET_CURRENT_USER':
+            //return new object
+            return {
+                ...state,
+                currentUser: action.payload
+            };
+            //if nothing matches return default 
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
