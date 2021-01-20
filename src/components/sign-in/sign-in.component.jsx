77b0490f2f2ad5572567707f3470d04b41d0db32
abutler6588 <abutler6588@gmail.com>
2021-01-20 09:29:14 -0600

    converted sign-up, sign-in, and menu-item components to styled components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './sign-in.styles.scss';
+import {SignInContainer, SignInTitle, ButtonsBarContainer} from './sign-in.styles';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../../components/custom-buttons/custom-button.component';
@@ -41,8 +41,8 @@ class SignIn extends React.Component{
 
     render(){
         return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
+            <SignInContainer>
+                <SignInTitle>I already have an account</SignInTitle>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
@@ -62,15 +62,15 @@ class SignIn extends React.Component{
                         label='password'
                         required 
                     />
-                    <div className='buttons'>
+                    <ButtonsBarContainer>
                         <CustomButton type='submit'>Sign In </CustomButton>
                         <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                             {''}
                             Sign In With Google {''}
                         </CustomButton>
-                    </div>
+                    </ButtonsBarContainer>
                 </form>
-            </div>
+            </SignInContainer>
         );
     }
 }
